86503c1b06db7312d8f9a2e3139242217d90f5f3
IdaliaO <157184583+IdaliaO@users.noreply.github.com>
2024-03-04 20:13:58 -0600

    practica finalizada

--- Tkinther/practica11.py
+++ Tkinther/practica11.py
@@ -1,4 +1,17 @@
-from tkinter import Tk, Frame
+from tkinter import Tk, Frame, Button, messagebox
+#metodos para mensaje
+def mostrarMensajes():
+    print(messagebox.showinfo('showinput','Information'))
+    print(messagebox.showerror('showwerror','Error'))
+    print(messagebox.showwarning('showwarning','Warning'))
+    print(messagebox.askokcancel(message = "¿Desea continuar?", title ="Soy el titulo" ))
+
+def addbtn():
+    botonVerde.config(text="+")
+    botonrosa= Button(seccion3, text="Nuevo", bg ="#EE1064")
+    botonrosa.configure(height=2, width=10)
+    botonrosa.pack()
+
 
 #1. Creamos la ventana
 Ventana = Tk() #uso de POO creando un Obj Ventana
@@ -14,5 +27,31 @@ seccion2.pack(expand=True, fill='both')
 seccion3 = Frame (Ventana, bg="blue")
 seccion3.pack(expand=True, fill='both')
 
+#3.posicionar Botones
+
+
+#place
+botonAzul= Button(seccion1, text="Azul", fg= "#50A6AB")
+botonAzul.place(x=60, y=60, width=100, height=30)
+
+#grid
+
+botonNegro = Button(seccion2, text="Negro", fg= "#5138AD", bg ="#090909")
+botonNegro.configure (height=2, width=10)
+botonNegro.grid(row=0, column=1)
+
+botonAmarillo= Button(seccion2, text="amarillo", bg="#4FEC51", command = mostrarMensajes)
+botonAmarillo.configure(height=2, width=10)
+botonAmarillo.grid(row=1, column=2)
+
+#pack
+botonVerde= Button(seccion3, text="verde", bg="#DDEE10", command=addbtn)
+botonVerde.configure(height=2, width=10)
+botonVerde.pack()
+
+
 #Ejecuta  la ventana 
 Ventana.mainloop()
+
+
+
