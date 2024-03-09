97e4a40f77a44d69d1e507010c6b60da59c543c7
IdaliaO <157184583+IdaliaO@users.noreply.github.com>
2024-03-08 20:30:54 -0600

    hasta aqui llegue

---
+++ Tkinther/practica13/clase.py
@@ -0,0 +1,10 @@
+def crearnumeros () -> list:
+    numeros = []
+    for i in range (0,10):
+        numeros.append (str(i))
+        return numeros
+def crearsimbolos(lenght=4):
+    password =""
+    simbolos = "!#$&"
+    
+    
---
+++ Tkinther/practica13/interfaz.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Frame, Button, Label, messagebox, Entry
+
+
+Ventana= Tk ()
+Ventana.title = ("Creacion de contraseñas")
+seccion1= Frame(Ventana)
+
+
+print(messagebox.askyesno(message="Usar caracteres especiales", title="Título"))
