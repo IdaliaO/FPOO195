fbdd3e884270bda6a7a2a276c29350b92a2f6711
IdaliaO <157184583+IdaliaO@users.noreply.github.com>
2024-03-11 20:23:44 -0600

    hasta aqui

---
+++ Examen2doP/clase.py
@@ -0,0 +1,17 @@
+class clase:
+    def __init__ (self,nombre, paterno, materno, año, carrera ):
+        self._nombre = nombre
+        self._paterno = paterno
+        self._materno =materno
+        self._año= año
+        self._carrera =carrera
+        self.mat()
+        
+        def mat(self):
+            
+            
+            
+              
+        
+        
+        
